5d08a0a9bcd1aba06e3dbee834897d278f6ff67c
Dean Patterson <deanp04@gmail.com>
2023-08-30 21:51:41 -0400

    removed old code commented out

--- src/Die.jsx
+++ src/Die.jsx
@@ -16,17 +16,3 @@ export default function Die(props) {
         </div>
     )
 }
-
-// return (
-//     <div 
-//         className="die-face" 
-//         style={styles}
-//         onClick={props.holdDice}
-//     >
-//         <h2 className={`die-num ${props.value === 1 ? "num-1" : props.value === 2 ? "num-2" : props.value === 3 ? "num-3" : props.value === 4 ? "num-4" : props.value === 5 ? "num-5" : "num-6"}`}>
-//             {Array.from({ length: props.value }).map((_, index) => (
-//                 <span key={index} className="dot"></span>
-//             ))}
-//         </h2>
-//     </div>
-// )
