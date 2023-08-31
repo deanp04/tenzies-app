e0a165171384d4e7d856210988c8d49d794bf3dc
Dean Patterson <deanp04@gmail.com>
2023-08-30 21:40:51 -0400

    dot positioning, corresponding

--- src/Die.jsx
+++ src/Die.jsx
@@ -6,11 +6,27 @@ export default function Die(props) {
     }
     return (
         <div 
-            className="die-face" 
+            className={`dice die-num ${props.value === 1 ? "num-1" : props.value === 2 ? "num-2" : props.value === 3 ? "num-3" : props.value === 4 ? "num-4" : props.value === 5 ? "num-5" : "num-6"}`} 
             style={styles}
             onClick={props.holdDice}
         >
-            <h2 className="die-num">{props.value}</h2>
+        {Array.from({ length: props.value }).map((_, index) => (
+            <span key={index} className="dot"></span>
+        ))}
         </div>
     )
 }
+
+// return (
+//     <div 
+//         className="die-face" 
+//         style={styles}
+//         onClick={props.holdDice}
+//     >
+//         <h2 className={`die-num ${props.value === 1 ? "num-1" : props.value === 2 ? "num-2" : props.value === 3 ? "num-3" : props.value === 4 ? "num-4" : props.value === 5 ? "num-5" : "num-6"}`}>
+//             {Array.from({ length: props.value }).map((_, index) => (
+//                 <span key={index} className="dot"></span>
+//             ))}
+//         </h2>
+//     </div>
+// )
